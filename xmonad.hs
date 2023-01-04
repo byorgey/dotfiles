2824ef0015af74c5506b8bbce1506833df87ae94
Brent Yorgey <byorgey@gmail.com>
2023-01-04 15:39:47 -0600

    update workspaces for Spring 23 classes

--- xmonad.hs
+++ xmonad.hs
@@ -217,16 +217,16 @@ myTopics host =
   -- , TI "heb" "documents/bible/study"
   --   (edit "~/documents/bible/study/Hebrews.tex" >>
   --    spawn "evince ~/documents/bible/study/Hebrews.pdf")
-  , TI "noah" "documents/noah/emacs" (edit "~/documents/noah/emacs/noah.txt")
+  -- , TI "noah" "documents/noah/emacs" (edit "~/documents/noah/emacs/noah.txt")
   , ti "150" "teaching/150"
   -- , ti "151" "teaching/151"
-  -- , ti "M240" "teaching/M240"
-  -- , ti "360" "teaching/360"
+  , ti "M240" "teaching/M240"
+  , ti "360" "teaching/360"
   -- , ti "CSO" "teaching/322"
   -- , ti "FP" "teaching/365"
-  , ti "382" "teaching/382"
-  , ti "410" "teaching/410"
-  , ti "exp" "teaching/explorations"
+  -- , ti "382" "teaching/382"
+  -- , ti "410" "teaching/410"
+  -- , ti "exp" "teaching/explorations"
   -- , ti "TEC" "teaching/TEC"
   , TI "joyal" "writing/translation/series-formelles" $ do
       edit "~/writing/translation/series-formelles/series-formelles.lhs"
